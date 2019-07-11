39951ee2eba2c118163a2e5a62354b8227fdf6dc
Hamid Paknahad <hp.paknahad@gmail.com>
2019-07-11 18:31:39 +0430

    add user CRUD.

---
+++ models/UserSearch.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\User;
+
+/**
+ * UserSearch represents the model behind the search form of `app\models\User`.
+ */
+class UserSearch extends User
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['personal_code', 'phone'], 'integer'],
+            [['first_name', 'last_name', 'email', 'lang'], 'safe'],
+            [['active', 'dead'], 'boolean'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = User::find();
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'personal_code' => $this->personal_code,
+            'phone' => $this->phone,
+            'active' => $this->active,
+            'dead' => $this->dead,
+        ]);
+
+        $query->andFilterWhere(['ilike', 'first_name', $this->first_name])
+            ->andFilterWhere(['ilike', 'last_name', $this->last_name])
+            ->andFilterWhere(['ilike', 'email', $this->email])
+            ->andFilterWhere(['ilike', 'lang', $this->lang]);
+
+        return $dataProvider;
+    }
+}
---
+++ views/user/index.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\UserSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Users';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create User', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'first_name:ntext',
+            'last_name:ntext',
+            'email:ntext',
+            'personal_code',
+            //'phone',
+            'active:boolean',
+            //'dead:boolean',
+            //'lang:ntext',
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
