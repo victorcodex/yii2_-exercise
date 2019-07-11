5ee55c7210df1b35c5a22805ef928748622b093c
Hamid Paknahad <hp.paknahad@gmail.com>
2019-07-11 17:37:42 +0430

    add Loan model.

---
+++ models/Loan.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "loans".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $amount
+ * @property string $interest
+ * @property int $duration
+ * @property string $start_date
+ * @property string $end_date
+ * @property int $campaign
+ * @property bool $status
+ *
+ * @property User $user
+ */
+class Loan extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'loan';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'amount', 'interest', 'duration', 'start_date', 'end_date', 'campaign'], 'required'],
+            [['user_id', 'duration', 'campaign'], 'integer'],
+            [['amount', 'interest'], 'number'],
+            [['start_date', 'end_date'], 'safe'],
+            [['status'], 'boolean'],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'amount' => 'Amount',
+            'interest' => 'Interest',
+            'duration' => 'Duration',
+            'start_date' => 'Start Date',
+            'end_date' => 'End Date',
+            'campaign' => 'Campaign',
+            'status' => 'Status',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::class, ['id' => 'user_id']);
+    }
+}
