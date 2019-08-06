607f5845e99916c3a6e05c2cd6a800044b6a7ff3
Victor Ifeanyi <vifeanyi33@gmail.com>
2019-08-06 07:59:57 +0100

    Modified the alignment of Loan view page title and action buttons

--- views/loan/view.php
+++ views/loan/view.php
@@ -13,18 +13,23 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="loan-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-warning']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <div class="section-header-panel">
+        <div class="pull-left">
+            <h1><?= Html::encode($this->title) ?></h1>
+        </div>
+        <div class="pull-right">
+            <p>
+                <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-warning']) ?>
+                <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+                    'class' => 'btn btn-danger',
+                    'data' => [
+                        'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                        'method' => 'post',
+                    ],
+                ]) ?>
+            </p>
+        </div>
+    </div>
 
     <?= DetailView::widget([
         'model' => $model,
